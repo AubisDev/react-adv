5a5b83019c8441f78afed70e9deda5839f1c1f8a
Aubis Sanchez <aubisst@gmail.com>
2022-07-25 18:25:45 -0400

    Lazyload Basico Listo

--- src/routes/Navigation.tsx
+++ src/routes/Navigation.tsx
@@ -1,47 +1,49 @@
+
+import { Suspense } from 'react';
 import {
   BrowserRouter as Router,
   Route,
   NavLink,
-  Routes
+  Routes,
+  Navigate
 } from 'react-router-dom';
 
-import { LazyPage1, LazyPage2,  LazyPage3 } from '../01-lazyload/pages';
 import logo from '../logo.svg';
-
+import { routes } from './routes';
 
 
 export const Navigation = () => {
   return (
-    <Router>
-      <div className="main-layout">
-        <nav>
-            <img src={ logo } alt="React Logo" />
-          <ul>
-            <li>
-              <NavLink to="/lazy1" className={(navData) => navData.isActive ? 'nav-active' : '' } >Lazy 1</NavLink>
-            </li>
-            <li>
-              <NavLink to="/lazy2"  className={(navData) => navData.isActive ? 'nav-active' : '' }>Lazy2</NavLink>
-            </li>
-            <li>
-              <NavLink to="/lazy3"  className={(navData) => navData.isActive ? 'nav-active' : '' }>Lazy3</NavLink>
-            </li>
-          </ul>
-        </nav>
+    <Suspense fallback={null}>
+      <Router> 
+        <div className="main-layout">
+          <nav>
+              <img src={ logo } alt="React Logo" />
+            <ul>
+              {
+                routes.map( ({ to,name}) => (
+                  <li key={to}>
+                    <NavLink to={to} className={({isActive}) => isActive ? 'nav-active' : '' } >{name}</NavLink>
+                  </li>
+                ))
+              }           
+            </ul>
+          </nav>
 
-        {/* A <Switch> looks through its children <Route>s and
-            renders the first one that matches the current URL. */}
-        <Routes>
-          <Route path="/lazy1" element={<LazyPage1/>}> 
-          </Route>
-          <Route path="/lazy2" element={<LazyPage2/>}>
-            <h1>Users</h1>
-          </Route>
-          <Route path="/lazy3"element={<LazyPage3/>}>
-            <h1>Home</h1> 
-          </Route>
-        </Routes>
-      </div>
-    </Router>
+          {/* A <Switch> looks through its children <Route>s and
+              renders the first one that matches the current URL. */}
+          <Routes>
+            {
+              routes.map( ({path, Component}) => (
+                <Route key={path} path={path} element={ <Component/> }>
+                </Route>  
+              ))
+            }
+            <Route path={'/*'} element={ <Navigate to={ routes[0].to} replace />} />
+          </Routes>
+        </div>
+      </Router>
+    </Suspense>
+   
   );
 }
